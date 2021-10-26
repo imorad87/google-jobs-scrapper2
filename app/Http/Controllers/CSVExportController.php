7b57d232c98dccba04fa55822abca60cc9eff55f
Islam Morad <imorad87@gmail.com>
2021-10-26 20:18:51 +0200

    csv extraction update

--- app/Http/Controllers/CSVExportController.php
+++ app/Http/Controllers/CSVExportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Exports\GoogleJobsExport;
+use App\Models\ApplyLink;
 use Carbon\Carbon;
 use App\Models\GoogleJob;
 use App\Models\ScrapingJob;
@@ -69,59 +70,96 @@ class CSVExportController extends Controller
 
     public function exportCsv(Request $request, $id)
     {
-        $now = Carbon::now();
-        $fileName = "googleJobsExport-$now.csv";
-        $scrapingJob = ScrapingJob::with(["googleJobs"])->find($id);
-        $googleJobs = $scrapingJob->googleJobs;
-        $file = null;
-        $headers = array(
-            "Content-type"        => "text/csv",
-            "Content-Disposition" => "attachment; filename=$fileName",
-            "Pragma"              => "no-cache",
-            "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
-            "Expires"             => "0"
-        );
+        try {
+            $now = Carbon::now();
+            $fileName = "googleJobsExport-$now.csv";
+            $scrapingJob = ScrapingJob::with(["googleJobs"])->find($id);
+            $googleJobs = $scrapingJob->googleJobs;
 
-        $columns = array('Id', 'JobTitle', 'JobType', 'JobDescription', 'CompanyName', 'Salary', 'Location', 'PostedSince', 'ContactName', 'ContactEmail', 'CreatedAt', 'UpdatedAt', 'ScrapingJobId');
+            // $jobWithLinks = $googleJobs[0]->load("applyLinks");
+            // $applyLinks = $jobWithLinks->applyLinks;
 
-        $callback = function () use ($googleJobs, $columns, $id) {
-            $file = fopen('php://output', 'w');
-            fputcsv($file, $columns, "~");
+            // $links = "";
 
-            foreach ($googleJobs as $googleJob) {
-                $row['Id']  =  $googleJob->id;
-                $row['JobTitle']    =  $googleJob->job_title;
-                $row['JobType']    =  $googleJob->job_type;
-                $row['JobDescription']    =  $googleJob->job_description;
-                $row['CompanyName']  =  $googleJob->company_name;
-                $row['Salary']  =   $googleJob->salary;
-                $row['Location']  =  $googleJob->location;
-                $row['PostedSince']  =   $googleJob->posted_since;
-                $row['ContactName']  =  $googleJob->contact_name;
-                $row['ContactEmail']  =  $googleJob->contact_email;
-                $row['CreatedAt']  =   $googleJob->created_at;
-                $row['UpdatedAt']  =   $googleJob->updated_at;
-                $row['ScrapingJobId']  =   $id;
+            // foreach ($applyLinks as $applyLink) {
+            //     //ddd([$applyLink->link, $applyLink->note]);
+            //     $links = $links . "~" . $applyLink->link . ":" .  $applyLink->note;
+            // }
+            // ddd($links);
+            $file = null;
+            $headers = array(
+                "Content-type"        => "text/csv",
+                "Content-Disposition" => "attachment; filename=$fileName",
+                "Pragma"              => "no-cache",
+                "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
+                "Expires"             => "0"
+            );
 
-                fputcsv($file, array(
-                    $row['Id'],
-                    $row['JobTitle'],
-                    $row['JobType'],
-                    $row['JobDescription'],
-                    $row['CompanyName'],
-                    $row['Salary'],
-                    $row['Location'],
-                    $row['PostedSince'],
-                    $row['ContactName'],
-                    $row['ContactEmail'],
-                    $row['CreatedAt'],
-                    $row['UpdatedAt'],
-                    $row['ScrapingJobId']
-                ), "~");
-            }
+            $columns = array('Id', 'JobTitle', 'JobType', 'JobDescription', 'CompanyName', 'Salary', 'Location', 'PostedSince', 'ContactName', 'ContactEmail', 'CreatedAt', 'UpdatedAt', 'ScrapingJobId', 'ApplyLinks');
 
-            fclose($file);
-        };
-        return response()->stream($callback, 200, $headers);
+            $callback = function () use ($googleJobs, $columns, $id) {
+                $file = fopen('php://output', 'w');
+                fputcsv($file, $columns, "~");
+
+
+
+                foreach ($googleJobs as $googleJob) {
+                    $jobWithLinks = $googleJob->load("applyLinks");
+
+                    $row['Id']                  =  $jobWithLinks->id;
+                    $row['JobTitle']            =  $jobWithLinks->job_title;
+                    $row['JobType']             =  $jobWithLinks->job_type;
+                    $row['JobDescription']      =  $jobWithLinks->job_description;
+                    $row['CompanyName']         =  $jobWithLinks->company_name;
+                    $row['Salary']              =  $jobWithLinks->salary;
+                    $row['Location']            =  $jobWithLinks->location;
+                    $row['PostedSince']         =  $jobWithLinks->posted_since;
+                    $row['ContactName']         =  $jobWithLinks->contact_name;
+                    $row['ContactEmail']        =  $jobWithLinks->contact_email;
+                    $row['CreatedAt']           =  $jobWithLinks->created_at;
+                    $row['UpdatedAt']           =  $jobWithLinks->updated_at;
+                    $row['ScrapingJobId']       =  $id;
+
+                    $applyLinks = $jobWithLinks->applyLinks;
+
+                    $links = "";
+                    $a = 0;
+                    foreach ($applyLinks as $applyLink) {
+                        $a++;
+                        if ($a > 1) {
+                            $links = $links . $applyLink->link . ": " .  $applyLink->note . "\n";
+                        } else {
+                            $links = $links . $applyLink->link . ": " .  $applyLink->note;
+                        }
+                    }
+
+                    $row["ApplyLinks"] = $links;
+
+                    //dd($links);
+
+                    fputcsv($file, array(
+                        $row['Id'],
+                        $row['JobTitle'],
+                        $row['JobType'],
+                        $row['JobDescription'],
+                        $row['CompanyName'],
+                        $row['Salary'],
+                        $row['Location'],
+                        $row['PostedSince'],
+                        $row['ContactName'],
+                        $row['ContactEmail'],
+                        $row['CreatedAt'],
+                        $row['UpdatedAt'],
+                        $row['ScrapingJobId'],
+                        $row["ApplyLinks"]
+                    ), "~");
+                }
+
+                fclose($file);
+            };
+            return response()->stream($callback, 200, $headers);
+        } catch (\Throwable $th) {
+            ddd($th);
+        }
     }
 }
